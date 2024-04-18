c6408d6bdd408a7f51f17cc7d0ebfbb64356034b
Noemie Thourault <noemie.thourault@etu.univ-lyon1.fr>
2024-04-18 12:02:54 +0200

    classe entite

--- src/Model/Entite.java
+++ src/Model/Entite.java
@@ -1,12 +1,17 @@
 package Model;
 
 public class Entite {
-
+    Case c;
+    
     public Entite() {
     }
 
     public void seDeplacerVers(Case c,Direction d){
-
+        Entite e = c.getEntite();
+        if(e == null){
+            e.pousser(d);
+        }
+        c.entrer(this, d);
     }
 
 }
