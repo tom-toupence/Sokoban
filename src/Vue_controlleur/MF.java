68787a8a466f1a093578de488d0eb32f784baf69
tom.toupence <tom.toupence@etu.univ-lyon1.fr>
2024-04-29 18:43:26 +0200

    hotfix

--- src/Vue_controlleur/MF.java
+++ src/Vue_controlleur/MF.java
@@ -22,42 +22,41 @@ public class MF extends JFrame implements Observer {
     }
 
     public void build() {
-    JPanel jp = new JPanel(new BorderLayout());
-    JPanel jpC = new JPanel(new GridLayout(jeu.SIZE_X, jeu.SIZE_Y));
-    this.setLocation(400, 50); // Centrer la fenêtre
-    setSize(750, 750);
-    // JPanel jpInfo = ....
-    jp.add(jpC, BorderLayout.CENTER);
-    // jp.add(jpInfo, BorderLayout.EAST);
-    Border blackline = BorderFactory.createLineBorder(Color.BLACK);
-    add(jp);
-
-    for (int i = 0; i < jeu.SIZE_X; i++) {
-        for (int j = 0; j < jeu.SIZE_Y; j++) {
-            tabC[i][j] = new JPanel();
-            tabC[i][j].setBorder(blackline);
-            jpC.add(tabC[i][j]);
-        }
-    }
-
-    // Parcourir le tableau tab et mettre à jour les couleurs en fonction du type de case
-    for (int x = 0; x < jeu.SIZE_X; x++) {
-        for (int y = 0; y < jeu.SIZE_Y; y++) {
-            if (jeu.tab[x][y] instanceof Mur) {
-                tabC[x][y].setBackground(Color.GRAY);
-            } else if (jeu.tab[x][y] instanceof Vide) {
-                tabC[x][y].setBackground(Color.WHITE);
+        JPanel jp = new JPanel(new BorderLayout());
+        JPanel jpC = new JPanel(new GridLayout(jeu.SIZE_X, jeu.SIZE_Y));
+        this.setLocation(400, 50); // Centrer la fenêtre
+        setSize(750, 750);
+        // JPanel jpInfo = ....
+        jp.add(jpC, BorderLayout.CENTER);
+        // jp.add(jpInfo, BorderLayout.EAST);
+        Border blackline = BorderFactory.createLineBorder(Color.BLACK);
+        add(jp);
+        for (int i = 0; i < jeu.SIZE_X; i++) {
+            for (int j = 0; j < jeu.SIZE_Y; j++) {
+                tabC[i][j] = new JPanel();
+                tabC[i][j].setBorder(blackline);
+                jpC.add(tabC[i][j]);
             }
-            Point positionHeros = jeu.h.getPosition();
-            if (positionHeros != null) {
-                tabC[positionHeros.x][positionHeros.y].setBackground(Color.BLUE);
-            }
-                           
         }
-    }
 
-    jp.setBorder(blackline);
-}
+        // Parcourir le tableau tab et mettre à jour les couleurs en fonction du type de case
+        for (int x = 0; x < jeu.SIZE_X; x++) {
+            for (int y = 0; y < jeu.SIZE_Y; y++) {
+                if (jeu.tab[x][y] instanceof Mur) {
+                    tabC[x][y].setBackground(Color.GRAY);
+                } else if (jeu.tab[x][y] instanceof Vide) {
+                    tabC[x][y].setBackground(Color.WHITE);
+                }
+                Point positionHeros = jeu.h.getPosition();
+                if (positionHeros != null) {
+                    tabC[positionHeros.x][positionHeros.y].setBackground(Color.BLUE);
+                }
+                            
+            }
+        }
+
+        jp.setBorder(blackline);
+    }
 
     /* public void addEC() {
         addKeyListener(new KeyAdapter() {
